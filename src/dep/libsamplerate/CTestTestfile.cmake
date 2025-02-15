549c8918ceb3845534657975c491acb93ff090d2
izzy2lost <izzynochill@gmail.com>
2025-02-15 01:40:03 -0500

    Add files via upload

---
+++ src/dep/libsamplerate/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: D:/a/Flamenco-UWP/Flamenco-UWP/src/dep/libsamplerate
+# Build directory: D:/a/Flamenco-UWP/Flamenco-UWP/src/dep/libsamplerate
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
