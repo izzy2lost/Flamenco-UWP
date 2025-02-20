08de350127b3667b6eb47b40ab7f027073ebcbb3
izzy2lost <izzynochill@gmail.com>
2025-02-19 21:34:35 -0500

    Update build-dependencies-windows-x64-uwp.bat

--- scripts/build-dependencies-windows-x64-uwp.bat
+++ scripts/build-dependencies-windows-x64-uwp.bat
@@ -1,11 +1,11 @@
 @echo off
 setlocal enabledelayedexpansion
 
-echo Setting UWP environment...
-if exist "%ProgramFiles%\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" (
-  call "%ProgramFiles%\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64 uwp
-) else if exist "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" (
-  call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64 uwp
+echo Setting environment...
+if exist "%ProgramFiles%\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat" (
+  call "%ProgramFiles%\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat" x64 uwp
+) else if exist "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" (
+  call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" x64 uwp
 ) else (
   echo Visual Studio 2022 not found.
   goto error
@@ -22,10 +22,17 @@ if defined DEBUG (
 pushd %~dp0
 set "SCRIPTDIR=%CD%"
 cd ..\dep\msvc
+
 mkdir deps-build
 cd deps-build || goto error
 set "BUILDDIR=%CD%"
 cd ..
+
+rem Check if deps-x64 exists, and if so, delete it
+if exist deps-x64 (
+    rmdir /s /q deps-x64
+)
+
 mkdir deps-x64
 cd deps-x64 || goto error
 set "INSTALLDIR=%CD%"
@@ -39,110 +46,70 @@ cd "%BUILDDIR%"
 
 set QT=6.6.2
 set QTMINOR=6.6
-set SDL=SDL2-2.30.1
 
-call :downloadfile "%SDL%.zip" "https://libsdl.org/release/%SDL%.zip" || goto error
-call :downloadfile "qtbase-everywhere-src-%QT%.zip" "https://download.qt.io/official_releases/qt/%QTMINOR%/%QT%/submodules/qtbase-everywhere-src-%QT%.zip" || goto error
-call :downloadfile "qtimageformats-everywhere-src-%QT%.zip" "https://download.qt.io/official_releases/qt/%QTMINOR%/%QT%/submodules/qtimageformats-everywhere-src-%QT%.zip" || goto error
-call :downloadfile "qtsvg-everywhere-src-%QT%.zip" "https://download.qt.io/official_releases/qt/%QTMINOR%/%QT%/submodules/qtsvg-everywhere-src-%QT%.zip" || goto error
-call :downloadfile "qttools-everywhere-src-%QT%.zip" "https://download.qt.io/official_releases/qt/%QTMINOR%/%QT%/submodules/qttools-everywhere-src-%QT%.zip" || goto error
-call :downloadfile "qttranslations-everywhere-src-%QT%.zip" "https://download.qt.io/official_releases/qt/%QTMINOR%/%QT%/submodules/qttranslations-everywhere-src-%QT%.zip" || goto error
+call :downloadfile "qtbase-everywhere-src-%QT%.zip" "https://github.com/izzy2lost/Flamenco-UWP/releases/download/6.6.2-base/qtbase-everywhere-src-6.6.2.zip" || goto error
+call :downloadfile "qtimageformats-everywhere-src-%QT%.zip" "https://github.com/izzy2lost/Flamenco-UWP/releases/download/6.6.2-image/qtimageformats-everywhere-src-6.6.2.zip" || goto error
+call :downloadfile "qtsvg-everywhere-src-%QT%.zip" "https://github.com/izzy2lost/Flamenco-UWP/releases/download/6.6.2-qtsvg/qtsvg-everywhere-src-6.6.2.zip" || goto error
+call :downloadfile "qttools-everywhere-src-%QT%.zip" "https://github.com/izzy2lost/Flamenco-UWP/releases/download/6.6.2-tools/qttools-everywhere-src-6.6.2.zip" || goto error
+call :downloadfile "qttranslations-everywhere-src-%QT%.zip" "https://github.com/izzy2lost/Flamenco-UWP/releases/download/6.6.2-trans/qttranslations-everywhere-src-6.6.2.zip" || goto error
 
 if %DEBUG%==1 (
-  echo Building debug and release libraries...
+  set QTBUILDSPEC=-DCMAKE_CONFIGURATION_TYPES="Release;Debug" -G "Ninja Multi-Config"
 ) else (
-  echo Building release libraries...
+  set QTBUILDSPEC=-DCMAKE_BUILD_TYPE=Release -G Ninja
 )
 
-set FORCEPDB=-DCMAKE_SHARED_LINKER_FLAGS_RELEASE="/DEBUG"
+set UWPFLAGS=-DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0 -DQT_NO_EXCEPTIONS=ON -DQT_BUILD_EXAMPLES=OFF -DQT_QMAKE_TARGET_MKSPEC=winrt-x64-msvc
 
-echo Building SDL...
-rmdir /S /Q "%SDL%"
-%SEVENZIP% x "%SDL%.zip" || goto error
-cd "%SDL%" || goto error
-
-rem Build SDL using MSBuild instead of CMake
-set SDL_SLN="VisualC-WinRT/SDL-UWP.sln"
-
-if %DEBUG%==1 (
-  echo Building SDL Debug...
-  msbuild %SDL_SLN% /p:Configuration=Debug /p:Platform=x64 || goto error
-  copy "VisualC-WinRT\x64\Debug\SDL-UWP\SDL2.dll" "%INSTALLDIR%\bin\" || goto error
-  copy "VisualC-WinRT\x64\Debug\SDL-UWP\SDL2.lib" "%INSTALLDIR%\lib\" || goto error
-  copy "VisualC-WinRT\x64\Debug\SDL-UWP\SDL2.pdb" "%INSTALLDIR%\bin\" || goto error
-)
-
-echo Building SDL Release...
-msbuild %SDL_SLN% /p:Configuration=Release /p:Platform=x64 || goto error
-copy "VisualC-WinRT\x64\Release\SDL-UWP\SDL2.dll" "%INSTALLDIR%\bin\" || goto error
-copy "VisualC-WinRT\x64\Release\SDL-UWP\SDL2.lib" "%INSTALLDIR%\lib\" || goto error
-copy "VisualC-WinRT\x64\Release\SDL-UWP\SDL2.pdb" "%INSTALLDIR%\bin\" || goto error
-
-cd .. || goto error
-
-set QTBUILDSPEC=^
-  -DQT_QMAKE_TARGET_MKSPEC=winrt-x64-msvc ^
-  -DCMAKE_SYSTEM_NAME=WindowsStore ^
-  -DCMAKE_SYSTEM_VERSION=10.0 ^
-  -G "Ninja"
-
-echo Building Qt base for UWP...
+echo Building Qt base...
 rmdir /S /Q "qtbase-everywhere-src-%QT%"
 %SEVENZIP% x "qtbase-everywhere-src-%QT%.zip" || goto error
 cd "qtbase-everywhere-src-%QT%" || goto error
-cmake -B build ^
-  -DFEATURE_sql=OFF ^
-  -DCMAKE_INSTALL_PREFIX="%INSTALLDIR%" ^
-  %FORCEPDB% ^
-  -DINPUT_gui=yes ^
-  -DINPUT_ssl=yes ^
-  -DINPUT_openssl=no ^
-  -DINPUT_schannel=yes ^
-  %QTBUILDSPEC% || goto error
+cmake -B build -DCMAKE_INSTALL_PREFIX="%INSTALLDIR%" %QTBUILDSPEC% %UWPFLAGS% -DINPUT_gui=yes -DINPUT_widgets=yes -DINPUT_ssl=yes -DINPUT_openssl=no -DINPUT_schannel=yes || goto error
 cmake --build build --parallel || goto error
 ninja -C build install || goto error
 cd .. || goto error
 
-echo Building Qt SVG for UWP...
+echo Building Qt SVG...
 rmdir /S /Q "qtsvg-everywhere-src-%QT%"
 %SEVENZIP% x "qtsvg-everywhere-src-%QT%.zip" || goto error
 cd "qtsvg-everywhere-src-%QT%" || goto error
 mkdir build || goto error
 cd build || goto error
-call "%INSTALLDIR%\bin\qt-configure-module.bat" .. -- %FORCEPDB% %QTBUILDSPEC% || goto error
+call "%INSTALLDIR%\bin\qt-configure-module.bat" .. -- %UWPFLAGS% || goto error
 cmake --build . --parallel || goto error
 ninja install || goto error
 cd ..\.. || goto error
 
-echo Building Qt Image Formats for UWP...
+echo Building Qt Image Formats...
 rmdir /S /Q "qtimageformats-everywhere-src-%QT%"
 %SEVENZIP% x "qtimageformats-everywhere-src-%QT%.zip" || goto error
 cd "qtimageformats-everywhere-src-%QT%" || goto error
 mkdir build || goto error
 cd build || goto error
-call "%INSTALLDIR%\bin\qt-configure-module.bat" .. -- %FORCEPDB% %QTBUILDSPEC% || goto error
+call "%INSTALLDIR%\bin\qt-configure-module.bat" .. -- %UWPFLAGS% || goto error
 cmake --build . --parallel || goto error
 ninja install || goto error
 cd ..\.. || goto error
 
-echo Building Qt Tools for UWP...
+echo Building Qt Tools...
 rmdir /S /Q "qttools-everywhere-src-%QT%"
 %SEVENZIP% x "qttools-everywhere-src-%QT%.zip" || goto error
 cd "qttools-everywhere-src-%QT%" || goto error
 mkdir build || goto error
 cd build || goto error
-call "%INSTALLDIR%\bin\qt-configure-module.bat" .. -- %FORCEPDB% %QTBUILDSPEC% -DFEATURE_assistant=OFF -DFEATURE_clang=OFF -DFEATURE_designer=OFF -DFEATURE_kmap2qmap=OFF -DFEATURE_pixeltool=OFF -DFEATURE_pkg_config=OFF || goto error
+call "%INSTALLDIR%\bin\qt-configure-module.bat" .. -- %UWPFLAGS% -DFEATURE_assistant=OFF -DFEATURE_clang=OFF -DFEATURE_designer=OFF -DFEATURE_kmap2qmap=OFF -DFEATURE_pixeltool=OFF -DFEATURE_pkg_config=OFF || goto error
 cmake --build . --parallel || goto error
 ninja install || goto error
 cd ..\.. || goto error
 
-echo Building Qt Translations for UWP...
+echo Building Qt Translations...
 rmdir /S /Q "qttranslations-everywhere-src-%QT%"
 %SEVENZIP% x "qttranslations-everywhere-src-%QT%.zip" || goto error
 cd "qttranslations-everywhere-src-%QT%" || goto error
 mkdir build || goto error
 cd build || goto error
-call "%INSTALLDIR%\bin\qt-configure-module.bat" .. -- %FORCEPDB% %QTBUILDSPEC% || goto error
+call "%INSTALLDIR%\bin\qt-configure-module.bat" .. -- %UWPFLAGS% || goto error
 cmake --build . --parallel || goto error
 ninja install || goto error
 cd ..\.. || goto error
@@ -151,7 +118,7 @@ echo Cleaning up...
 cd ..
 rd /S /Q deps-build
 
-echo UWP build completed successfully.
+echo Exiting with success.
 exit 0
 
 :error
